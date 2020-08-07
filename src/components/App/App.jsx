7d390aa80cca99bd5f1ca58e2bc14b8f64b46dc4
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-08-07 10:27:40 -0700

    remove module

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CampsiteList from './CampsiteList/CampsiteList';
 import './reset.css';
-import CampsiteAvailability from '../CampsiteAvailability/CampsiteAvailability';
+// import CampsiteAvailability from '../CampsiteAvailability/CampsiteAvailability';
 
 export default function App() {
   return (
